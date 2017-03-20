474ff1890f99d8f33742a836af95df1d4dad7fa2
Jordan Brauer <esales@midcanadafasteners.com>
2017-03-20 11:53:57 -0500

    Fix: order of options change - required at top

--- src/app.js
+++ src/app.js
@@ -5,8 +5,8 @@ let googleMaps;
 function initMap() {
   let options = {
     center: new google.maps.LatLng(49.895136, -97.1383743999999),
-    scrollwheel: false,
     zoom: 11,
+    scrollwheel: false,
   }
 
   return googleMaps = new Map(Locations, options);
