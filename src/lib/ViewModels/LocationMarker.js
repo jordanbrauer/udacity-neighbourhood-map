cca4b5391e26b1a2a640853d140851d57c53bbee
jordanbrauer <jbrauer.inc@gmail.com>
2017-03-29 21:39:34 -0500

    Feat: .animate wrapper method and FourSquare API implemented

--- src/lib/ViewModels/LocationMarker.js
+++ src/lib/ViewModels/LocationMarker.js
@@ -18,7 +18,42 @@ const LocationMarker = function (map, data) {
   const self = this;
 
   /** @private */
-  const init = function () {};
+  const init = function () {
+    const request = $.get(`${self.api.base_uri}/${self.api.end_point}`);
+
+    request.done(function (res) {
+      self.api.status = res.meta.code;
+
+      const venue = res.response.venues[0];
+
+      self.url = venue.url;
+      self.street = venue.location.formattedAddress[0];
+      self.city = venue.location.formattedAddress[1];
+      self.phoneNumber = venue.contact.formattedPhone;
+
+      self.content =`
+      <h4><a href="${self.url}" target="_blank">${self.name}</a></h4>
+      <p>${self.street}, ${self.city}</p>
+      <p>${self.lat}, ${self.lng}</p>
+      <p>${self.phoneNumber}</p>
+      `;
+    });
+
+    request.fail(function (err) {
+      self.api.status = err.status;
+
+      console.error(`ERROR ${err.status} (${err.statusText}): ${err.responseJSON.meta.errorDetail}`);
+
+      self.content = '<span style="color: red;">Oops, it appears something has gone wrong!</span>';
+    });
+    console.log(self);
+  };
+
+  /** @private */
+  const CLIENT_ID = 'E45RTSS15B1T3HKN1R1Y03TLIV3CHBPFCAY52CC1CFAWT3H5';
+
+  /** @private */
+  const CLIENT_SECRET = 'BKMWVMAPHQ5LLDGWDVWKEAJ35USFQPFLI0NKBJ4BBPECHOHZ';
 
   /**
    * @memberof LocationMarker
@@ -56,6 +91,30 @@ const LocationMarker = function (map, data) {
    */
   this.longitude = this.lng;
 
+  this.api = {
+    base_uri: 'https://api.foursquare.com/v2',
+    end_point: `venues/search?ll=${self.lat},${self.lng}&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&v=20170329`,
+    status: null,
+    // request: null,
+  };
+
+  this.url;
+  this.street;
+  this.city;
+  this.phoneNumber;
+
+  /**
+   * @memberof LocationMarker
+   * @property {string} content - Content to be placed in the `infoWindow`s.
+   */
+  this.content;
+
+  /**
+  * @memberof LocationMarker
+  * @property {object} infoWindow - The info window UI element that pops up when a marker or list item is clicked.
+  */
+  this.infoWindow = new google.maps.InfoWindow();
+
   /**
    * @memberof LocationMarker
    * @property {object} marker - The actual marker that is placed on the map.
@@ -63,29 +122,48 @@ const LocationMarker = function (map, data) {
   this.marker = new google.maps.Marker({
     map: self.map,
     position: new google.maps.LatLng(self.lat, self.lng),
-    title: self.name
+    title: self.name,
   });
 
-  /**
-   * @memberof LocationMarker
-   * @property {string} content - Content to be placed in the `infoWindow`s.
-   */
-  this.content =`
-  <h3>${this.name}</h3>
-  <p>${this.lat}, ${this.lng}</p>
-  `;
-
-  /**
-   * @memberof LocationMarker
-   * @property {object} infoWindow - The info window UI element that pops up when a marker or list item is clicked.
-   */
-  this.infoWindow = new google.maps.InfoWindow({
-    content: self.content
-  });
-
-  // Add an event listener to our marker, listening for a 'click',
-  // open the markers' infoWindow when heard.
+  // Click event listener
   this.marker.addListener('click', function (){
+    if (!self.infoWindow.content) {
+      self.infoWindow.setContent(self.content);
+    }
+
+    self.animate('bounce', 2150);
+
     self.infoWindow.open(this.map, this);
   });
+
+  // Call object constructor
+  init();
+};
+
+/**
+ * @memberof LocationMarker
+ * @method animate
+ * @param {string} animation - The type of animation to be executed on the marker - see a list Google Maps marker animations for more options.
+ * @param {integer} duration - (optional) Duration of the animation in miliseconds.
+ */
+LocationMarker.prototype.animate = function (animation, duration = null) {
+  const self = this;
+  let ANIMATION;
+
+  switch (animation.toUpperCase()) {
+    case 'BOUNCE':
+      ANIMATION = google.maps.Animation.BOUNCE;
+      break;
+    case 'DROP':
+      ANIMATION = google.maps.Animation.DROP;
+      break;
+    default:
+      ANIMATION = null;
+  }
+
+  self.marker.setAnimation(ANIMATION);
+
+  if (duration != null) {
+    setTimeout(function () { self.marker.setAnimation(null); }, duration);
+  }
 };
